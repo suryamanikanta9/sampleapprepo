d9151326bbec2a5e1719ed002ef2ea452d3d3259
suryamanikanta9 <gsmk9669@gmail.com>
2021-09-20 11:04:31 +0530

    boot updated

--- sampleapp/src/main/java/org/cap/demo/BootClass.java
+++ sampleapp/src/main/java/org/cap/demo/BootClass.java
@@ -8,8 +8,8 @@ public class BootClass {
 
 	}
 	public static void show() {
-		int a[]= {1,2,3,4};
-		for(int i=0;i<20;i++) {
+		int a[]= {20,21,23,24};
+		for(int i:a) {
 			System.out.println(i);
 		}
 		
